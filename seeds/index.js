5f087a136ec9148e93d38e9596ab19776eeb2482
Lusine Papanyan <lpapanyan9611@gmail.com>
2023-05-30 15:13:49 +0400

    added hideous random image

--- seeds/index.js
+++ seeds/index.js
@@ -24,9 +24,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 51; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "lorem ipsum dolor sit u esim inch, aunt multum legendum esim inch. multum, non multa",
+      price,
     });
     await camp.save();
   }
